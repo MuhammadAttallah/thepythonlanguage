1de5ee0a022257c37573ce9fad94f35dcb3aaa79
Muhammad Attallah <muhammad.hermes@gmail.com>
2022-03-19 13:50:00 +0200

    Annotate all outputs

--- apps/DNA-Toolkit/main.py
+++ apps/DNA-Toolkit/main.py
@@ -4,12 +4,15 @@ import random
 
 #rndDNAstr = "ATTTCcgtT"
 
-# generate random string
+# generate random string for testing
 rndDNAstr = ''.join([random.choice(nucleotides) for nuc in range(50)])
 
-# validate DNA sequence
-#print(validateSeq(rndDNAstr))
 DNAstr = validateSeq(rndDNAstr) # make sure working with a validated string
 
-# count nucleotides frequency
-print(countNucFreq(DNAstr))
+print(f"\nSequence: {DNAstr}\n")
+print(f"[1] + Sequence Length: {len(DNAstr)}\n")
+print(f"[2] + Nucleotide Frequency: {countNucFreq(DNAstr)}\n")
+print(f"[3] + DNA/RNA Transcription: {transcription(DNAstr)}\n")
+print(f"[4] + DNA String + Reverse Complement:\n5' {DNAstr} 3'")
+print(f"   {''.join(['|' for c in range (len(DNAstr))])}")
+print(f"3' {reverse_complement(DNAstr)} 5'\n")
