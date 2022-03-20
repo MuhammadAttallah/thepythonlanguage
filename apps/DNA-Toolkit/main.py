e989ba3a7ac82e79008f3199f7f005cf4bd4cc46
Muhammad Attallah <muhammad.hermes@gmail.com>
2022-03-20 11:22:01 +0200

    Test colored functionality

--- apps/DNA-Toolkit/main.py
+++ apps/DNA-Toolkit/main.py
@@ -1,5 +1,6 @@
 # code testing
 from DNAToolkit import *
+from utilities import colored
 import random
 
 #rndDNAstr = "ATTTCcgtT"
@@ -9,10 +10,10 @@ rndDNAstr = ''.join([random.choice(nucleotides) for nuc in range(50)])
 
 DNAstr = validateSeq(rndDNAstr) # make sure working with a validated string
 
-print(f"\nSequence: {DNAstr}\n")
+print(f"\nSequence: {colored(DNAstr)}\n")
 print(f"[1] + Sequence Length: {len(DNAstr)}\n")
-print(f"[2] + Nucleotide Frequency: {countNucFreq(DNAstr)}\n")
-print(f"[3] + DNA/RNA Transcription: {transcription(DNAstr)}\n")
-print(f"[4] + DNA String + Reverse Complement:\n5' {DNAstr} 3'")
+print(colored(f"[2] + Nucleotide Frequency: {countNucFreq(DNAstr)}\n"))
+print(f"[3] + DNA/RNA Transcription: {colored(transcription(DNAstr))}\n")
+print(f"[4] + DNA String + Reverse Complement:\n5' {colored(DNAstr)} 3'")
 print(f"   {''.join(['|' for c in range (len(DNAstr))])}")
-print(f"3' {reverse_complement(DNAstr)} 5'\n")
+print(f"3' {colored(reverse_complement(DNAstr))} 5'\n")
