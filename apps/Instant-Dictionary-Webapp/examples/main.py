eba4a29b69307641f98f460e85fb8ed0e9d2340b
Muhammad Attallah <muhammad.hermes@gmail.com>
2022-06-06 13:44:18 +0200

    Handle events by functions

--- apps/Instant-Dictionary-Webapp/examples/main.py
+++ apps/Instant-Dictionary-Webapp/examples/main.py
@@ -6,22 +6,27 @@ def home():
     wp = jp.WebPage()
     div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
 
-    div1 = jp.Div(a=div, classes='grid grid-cols-3 gap-4 p-4 border border-black')
-    jp.Input(a=div1, placeholder='Enter first value', classes='form-input')
-    jp.Input(a=div1, placeholder='Enter second value', classes='form-input')
-    jp.Div(a=div1, text='Results goes here....', classes='text-gray-600')
+    div1 = jp.Div(a=div, classes='grid grid-cols-3 gap-4 p-4')
+    in_1 = jp.Input(a=div1, placeholder='Enter first value', classes='form-input')
+    in_2 = jp.Input(a=div1, placeholder='Enter second value', classes='form-input')
+    d_output = jp.Div(a=div1, text='Results goes here....', classes='text-gray-600')
     jp.Div(a=div1, text='Just another div....', classes='text-gray-600')
     jp.Div(a=div1, text='Yet another div....', classes='text-gray-600')
 
     div2 = jp.Div(a=div, classes='grid grid-cols-2 gap-4')
-    jp.Button(a=div2, text='Calculate', classes='border border-blue-500 m- py-1 px-4 rounded'
-                                              'text-blue-600 hover:bg-red-500 hover:text-white')
+    jp.Button(a=div2, text='Calculate', click=sum_up, in1=in_1, in2=in_2, d=d_output,
+              classes='border border-blue-500 m- py-1 px-4 rounded'
+              'text-blue-600 hover:bg-red-500 hover:text-white')
 
     jp.Div(a=div2, text='I am a cool interactive div!!')
     # jp.Div(a=wp, text='Hello world/home', 
     #        classes='text-orange-900 bg-yellow-500 font-serif text-lg')
     # jp.Div(a=wp, text='Hello agin/home!!')
     return wp
+
+def sum_up(widget, msg):
+    sum = float(widget.in1.value) + float(widget.in2.value)
+    widget.d.text = sum
 
 # @jp.SetRoute('/about')
 # def about():
